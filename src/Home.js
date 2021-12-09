16d2d1ddcc0019ff134454f2265736bcc8bbcc17
Mohammad Masudul Alam <malam172034@bscse.uiu.ac.bd>
2021-12-09 13:45:00 +0600

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log("Hello there")
+    }
+
+    const handleClickAgain = (name) => {
+        console.log("Hello," +name)
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick = {handleClick}>Click me</button>
+            <button onClick = {()=> handleClickAgain("Mofiz")}>Click me again</button>
         </div>
      );
 }
