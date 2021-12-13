349dc7f068123a08e9ffc50152307ee6e7680626
Mohammad Masudul Alam <malam172034@bscse.uiu.ac.bd>
2021-12-13 13:16:31 +0600

    reusing components

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,16 @@ import { useState } from "react";
 const Home = () => {
 
     const [blogs, setBlogs] = useState( [
-        { id : 1, title : 'My blog-1', body : 'This is first blog body', author : 'Snoop Dogg' },
-        { id : 2, title : 'My blog-2', body : 'This is second blog body', author : '50 Cent' }
+        { id : 1, title : 'blog-1', body : 'This is first blog body', author : 'Snoop Dogg' },
+        { id : 2, title : 'blog-2', body : 'This is second blog body', author : '50 Cent' },
+        { id : 3, title : 'blog-3', body : 'This is third blog body', author : 'Snoop Dogg' }
 
     ]);
 
     return ( 
         <div className="home">
             <BlogList blogs = {blogs} title = "All blogs"/>
+            <BlogList blogs = {blogs.filter( (blog) => blog.author === 'Snoop Dogg')} title = "Mario's blogs"/>
         </div>
      );
 }
