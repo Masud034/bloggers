5a86e3f32156706c4f710f6fb3ef8edbf7a9f4e7
Mohammad Masudul Alam <malam172034@bscse.uiu.ac.bd>
2021-12-14 15:38:18 +0600

    custom reusable hook

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,9 @@
 import BlogList from "./BlogList.";
-import { useState, useEffect } from "react";
+import useFetch from "./useFetch";
+
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogss')
-          .then(res => {
-              if( !res.ok) {
-                  throw Error("Could not fetch");
-              }
-            return res.json();
-          })
-          .then(data => {
-            setBlogs(data);
-            setIsPending(false);
-          })
-          .catch(err => {
-              setError(err.message);
-              setIsPending(false);
-          })
-      }, [])
+    const {data : blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
 
     return ( 
         <div className="home">
