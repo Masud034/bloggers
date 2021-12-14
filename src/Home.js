1c9587955dfae23412b15ccbb284b304d8bb6ffb
Mohammad Masudul Alam <malam172034@bscse.uiu.ac.bd>
2021-12-14 13:29:26 +0600

    handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -4,19 +4,29 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        fetch('http://localhost:8000/blogss')
           .then(res => {
+              if( !res.ok) {
+                  throw Error("Could not fetch");
+              }
             return res.json();
           })
           .then(data => {
             setBlogs(data);
             setIsPending(false);
           })
+          .catch(err => {
+              setError(err.message);
+              setIsPending(false);
+          })
       }, [])
 
     return ( 
         <div className="home">
+            { error && <div className="err"> {error} </div> }
             { isPending && <div className="loading">Loading...</div> }
             { blogs && <BlogList blogs = {blogs} title = "All blogs" /> }
         </div>
