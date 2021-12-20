735b07232d78e3a7b5e7f7fb528069184c9bd0a1
Mohammad Masudul Alam <malam172034@bscse.uiu.ac.bd>
2021-12-20 18:12:44 +0600

    submit events

--- src/Create.js
+++ src/Create.js
@@ -6,10 +6,15 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+    }
+
     return ( 
         <div className="create">
             <h2>Add new blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog Title: </label>
                 <input 
                     type = 'text'
